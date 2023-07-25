5c7c52c57486bb2c7fa244802fbbd354870ed9f3
gaganjot09 <123991934+gaganjot09@users.noreply.github.com>
2023-07-25 21:54:45 +0530

    Update demo1.java

--- java_bank_system/demo1.java
+++ java_bank_system/demo1.java
@@ -75,12 +75,48 @@ class Bank implements Serializable {
         this.accounts = new ArrayList<>();
         this.loggedInUser = null;
     }
+     )
+
+    public boolean isUsernameTaken(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void createUser(String username, String password) {
+        if (isUsernameTaken(username)) {
+            System.out.println("Username is already taken. Please provide a different username.");
+            return;
+        }
+
         User newUser = new User(username, password, false);
         users.add(newUser);
         System.out.println("User created successfully.");
     }
+
+    public void createAdminUserAccount(String username, String password, String accountType) {
+        if (isUsernameTaken(username)) {
+            System.out.println("Username is already taken. Please provide a different username.");
+            return;
+        }
+
+        User newUser = new User(username, password, false);
+        users.add(newUser);
+
+        int accountNumber = accounts.size() + 1;
+        Account newAccount = new Account(accountNumber, accountType);
+        accounts.add(newAccount);
+        System.out.println("User account created successfully. Account number: " + accountNumber);
+
+        // Display the newly created user's credentials
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+
+    
 
     public User loginUser(String username, String password) {
         for (User user : users) {
@@ -105,7 +141,8 @@ class Bank implements Serializable {
         accounts.add(newAccount);
         System.out.println("Account created successfully. Account number: " + accountNumber);
     }
-
+    // New method for creating a user account by the admin
+    
     public Account findAccountByNumber(int accountNumber) {
         for (Account account : accounts) {
             if (account.getAccountNumber() == accountNumber) {
@@ -193,9 +230,56 @@ class Bank implements Serializable {
             System.out.println("Error loading data: " + e.getMessage());
         }
     }
+    // New method for creating a user account
+    public void createUserAccount(String username, String password) {
+        User newUser = new User(username, password, false);
+        users.add(newUser);
+        System.out.println("User account created successfully.");
+    }
+
+    // New method for blocking a user account (Admin functionality)
+    public void blockUserAccount(String username) {
+        User userToBlock = findUserByUsername(username);
+        if (userToBlock != null && !userToBlock.isAdmin()) {
+            users.remove(userToBlock);
+            System.out.println("User account blocked successfully.");
+        } else {
+            System.out.println("User not found or cannot block admin accounts.");
+        }
+    }
+
+    // Helper method to find a user by their username
+    private User findUserByUsername(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // New method for deleting a user account (Admin functionality)
+    public void deleteUserAccount(String username, int accountNumber) {
+    User userToDelete = findUserByUsername(username);
+    if (userToDelete != null && !userToDelete.isAdmin()) {
+        // Check if the user owns the account before deleting
+        for (Account account : accounts) {
+            if (account.getAccountNumber() == accountNumber) {
+                users.remove(userToDelete);
+                System.out.println("User account deleted successfully.");
+                return;
+            }
+        }
+        System.out.println("User does not own the specified account.");
+    } else {
+        System.out.println("User not found or cannot delete admin accounts.");
+    }
 }
 
-public class demo1 {
+}
+
+
+public class demo4 {
     public static void main(String[] args) {
         Bank bank = new Bank();
         Scanner scanner = new Scanner(System.in);
@@ -210,8 +294,7 @@ public class demo1 {
             System.out.println("0. Exit");
 
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
-
+            scanner.nextLine(); 
             switch (choice) {
                 case 1:
                     System.out.println("User Login");
@@ -224,25 +307,24 @@ public class demo1 {
                         boolean userLogout = false;
                         while (!userLogout) {
                             System.out.println("\nPlease select an option:");
-                            System.out.println("1. Create Account");
-                            System.out.println("2. Deposit");
-                            System.out.println("3. Withdraw");
-                            System.out.println("4. Transfer");
-                            System.out.println("5. Display Account Summary");
-                            System.out.println("6. Logout");
+                            // System.out.println("1. Create Account");
+                            System.out.println("1. Deposit");
+                            System.out.println("2. Withdraw");
+                            System.out.println("3. Transfer");
+                            System.out.println("4. Display Account Summary");
+                            System.out.println("5. Logout");
                             System.out.println("0. Exit");
 
                             int userChoice = scanner.nextInt();
-                            scanner.nextLine(); // Consume the newline character
-
+                            scanner.nextLine(); 
                             switch (userChoice) {
-                                case 1:
+                                case 9:
                                     System.out.println("Create Account");
                                     System.out.println("Select account type:");
                                     System.out.println("1. Savings Account");
                                     System.out.println("2. Checking Account");
                                     int accountChoice = scanner.nextInt();
-                                    scanner.nextLine(); // Consume the newline character
+                                    scanner.nextLine(); 
                                     String accountType;
                                     if (accountChoice == 1) {
                                         accountType = "Savings Account";
@@ -254,25 +336,25 @@ public class demo1 {
                                     }
                                     bank.createAccount(accountType);
                                     break;
-                                case 2:
+                                case 1:
                                     System.out.println("Deposit");
                                     System.out.println("Enter account number:");
                                     int depositAccountNumber = scanner.nextInt();
                                     System.out.println("Enter amount to deposit:");
                                     double depositAmount = scanner.nextDouble();
-                                    scanner.nextLine(); // Consume the newline character
+                                    scanner.nextLine(); 
                                     bank.deposit(depositAccountNumber, depositAmount);
                                     break;
-                                case 3:
+                                case 2:
                                     System.out.println("Withdraw");
                                     System.out.println("Enter account number:");
                                     int withdrawAccountNumber = scanner.nextInt();
                                     System.out.println("Enter amount to withdraw:");
                                     double withdrawAmount = scanner.nextDouble();
-                                    scanner.nextLine(); // Consume the newline character
+                                    scanner.nextLine(); 
                                     bank.withdraw(withdrawAccountNumber, withdrawAmount);
                                     break;
-                                case 4:
+                                case 3:
                                     System.out.println("Transfer");
                                     System.out.println("Enter source account number:");
                                     int transferSourceAccountNumber = scanner.nextInt();
@@ -280,17 +362,17 @@ public class demo1 {
                                     int transferDestinationAccountNumber = scanner.nextInt();
                                     System.out.println("Enter amount to transfer:");
                                     double transferAmount = scanner.nextDouble();
-                                    scanner.nextLine(); // Consume the newline character
+                                    scanner.nextLine(); 
                                     bank.transfer(transferSourceAccountNumber, transferDestinationAccountNumber, transferAmount);
                                     break;
-                                case 5:
+                                case 4:
                                     System.out.println("Display Account Summary");
                                     System.out.println("Enter account number:");
                                     int accountNumber = scanner.nextInt();
-                                    scanner.nextLine(); // Consume the newline character
+                                    scanner.nextLine(); 
                                     bank.displayAccountSummary(accountNumber);
                                     break;
-                                case 6:
+                                case 5:
                                     bank.logoutUser();
                                     System.out.println("User logged out successfully.");
                                     userLogout = true;
@@ -326,19 +408,25 @@ public class demo1 {
                                 System.out.println("4. Transfer");
                                 System.out.println("5. Display Account Summary");
                                 System.out.println("6. Logout");
+                                // System.out.println("7. Block User Account");
+                                System.out.println("7. Delete User Account");
                                 System.out.println("0. Exit");
 
                                 int adminChoice = scanner.nextInt();
-                                scanner.nextLine(); // Consume the newline character
+                                scanner.nextLine(); 
 
                                 switch (adminChoice) {
                                     case 1:
                                         System.out.println("Create Account");
+                                        System.out.println("Enter username for the new user account:");
+                                        String newUserUsername = scanner.nextLine();
+                                        System.out.println("Enter password for the new user account:");
+                                        String newUserPassword = scanner.nextLine();
                                         System.out.println("Select account type:");
                                         System.out.println("1. Savings Account");
                                         System.out.println("2. Checking Account");
                                         int accountChoice = scanner.nextInt();
-                                        scanner.nextLine(); // Consume the newline character
+                                        scanner.nextLine(); 
                                         String accountType;
                                         if (accountChoice == 1) {
                                             accountType = "Savings Account";
@@ -348,7 +436,7 @@ public class demo1 {
                                             System.out.println("Invalid choice.");
                                             continue;
                                         }
-                                        bank.createAccount(accountType);
+                                        bank.createAdminUserAccount(newUserUsername, newUserPassword,accountType);
                                         break;
                                     case 2:
                                         System.out.println("Deposit");
@@ -356,7 +444,7 @@ public class demo1 {
                                         int depositAccountNumber = scanner.nextInt();
                                         System.out.println("Enter amount to deposit:");
                                         double depositAmount = scanner.nextDouble();
-                                        scanner.nextLine(); // Consume the newline character
+                                        scanner.nextLine(); 
                                         bank.deposit(depositAccountNumber, depositAmount);
                                         break;
                                     case 3:
@@ -365,7 +453,7 @@ public class demo1 {
                                         int withdrawAccountNumber = scanner.nextInt();
                                         System.out.println("Enter amount to withdraw:");
                                         double withdrawAmount = scanner.nextDouble();
-                                        scanner.nextLine(); // Consume the newline character
+                                        scanner.nextLine(); 
                                         bank.withdraw(withdrawAccountNumber, withdrawAmount);
                                         break;
                                     case 4:
@@ -376,20 +464,35 @@ public class demo1 {
                                         int transferDestinationAccountNumber = scanner.nextInt();
                                         System.out.println("Enter amount to transfer:");
                                         double transferAmount = scanner.nextDouble();
-                                        scanner.nextLine(); // Consume the newline character
+                                        scanner.nextLine(); 
                                         bank.transfer(transferSourceAccountNumber, transferDestinationAccountNumber, transferAmount);
                                         break;
                                     case 5:
                                         System.out.println("Display Account Summary");
                                         System.out.println("Enter account number:");
                                         int accountNumber = scanner.nextInt();
-                                        scanner.nextLine(); // Consume the newline character
+                                        scanner.nextLine(); 
                                         bank.displayAccountSummary(accountNumber);
                                         break;
                                     case 6:
                                         bank.logoutUser();
                                         System.out.println("Admin logged out successfully.");
                                         adminLogout = true;
+                                        break;
+                                    case 8:
+                                        System.out.println("Block User Account");
+                                        System.out.println("Enter username to block:");
+                                        String userToBlock = scanner.nextLine();
+                                        bank.blockUserAccount(userToBlock);
+                                        break;
+                                    case 7:
+                                        System.out.println("Delete User Account");
+                                        System.out.println("Enter username to delete:");
+                                        String userToDelete = scanner.nextLine();
+                                        System.out.println("Enter account number to delete:");
+                                        int accountNumberToDelete = scanner.nextInt();
+                                        scanner.nextLine();
+                                        bank.deleteUserAccount(userToDelete, accountNumberToDelete); 
                                         break;
                                     case 0:
                                         exit = true;
